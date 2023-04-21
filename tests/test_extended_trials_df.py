fc42a8814e031bd49c151fa280bd5a5fe1bdb0c4
Chris Mochizuki <chrism@alleninstitute.org>
2023-04-21 13:21:30 -0700

    temp

--- tests/test_extended_trials_df.py
+++ tests/test_extended_trials_df.py
@@ -1,3 +1,14 @@
+def has_response_before_window(change_time, lick_times, response_window):
+    response_window_lower = change_time + response_window[0]
+    return len(list(filter(lambda value: value < response_window_lower, lick_times))) > 0
+
+
+def has_response_within_window(change_time, lick_times, response_window):
+    response_window_lower = change_time + response_window[0]
+    response_window_upper = change_time + response_window[1]
+    return len(list(filter(lambda value: value >= response_window_lower and value <= response_window_upper, lick_times))) > 0
+
+
 def test_response_types(raw, extended_trials_df):
     response_window_lower, response_window_upper = raw[
         "items"]["behavior"]["params"]["response_window"]
