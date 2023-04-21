b2ade0da3d83d44e827ea75f8e9aae215d224206
Chris Mochizuki <chrism@alleninstitute.org>
2023-04-21 12:52:54 -0700

    temp

--- tests/test_extended_trials_df.py
+++ tests/test_extended_trials_df.py
@@ -1,5 +1,6 @@
 def test_response_types(raw, extended_trials_df):
-    raw_params = raw["items"]["behavior"]["params"]
-    raise Exception(raw["response_window"])
+    response_window = raw["items"]["behavior"]["params"]["response_window"]
+    print(extended_trials_df.head())
+    raise Exception(response_window)
     for trial in raw["items"]["behavior"]["trial_log"]:
         events = trial["events"]
