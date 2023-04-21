08021dd5f88f8303df7ec88cc9fbfb34f4a85f92
Chris Mochizuki <chrism@alleninstitute.org>
2023-04-21 12:43:31 -0700

    more testa

---
+++ tests/test_extended_trials_df.py
@@ -0,0 +1,5 @@
+def test_response_types(raw, extended_trials_df):
+    raw_params = raw["items"]["behavior"]["params"]
+    raise Exception(raw["response_time"])
+    for trial in raw["items"]["behavior"]["trial_log"]:
+        events = trial["events"]
