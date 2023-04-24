1f9019f032f04b89193ca2040a8a4c6e75a118dd
Chris Mochizuki <chrism@alleninstitute.org>
2023-04-24 12:02:28 -0700

    cleanup tests

--- tests/test_extended_trials_df.py
+++ tests/test_extended_trials_df.py
@@ -16,41 +16,6 @@ def has_response_within_window(change_time, lick_times, response_window):
     return len(list(filter(lambda value: value >= response_window_lower and value <= response_window_upper, lick_times))) > 0
 
 
-# def test_response_types(raw, extended_trials_df):
-#     response_window = raw["items"]["behavior"]["params"]["response_window"]
-#     for idx, row in extended_trials_df.iterrows():
-#         raw_log = raw["items"]["behavior"]["trial_log"][idx]
-#         trial_type = row["trial_type"]
-#         change_time = row["change_time"]
-#         lick_times = row["lick_times"]
-#         response_type = row["response_type"]
-#         licks_enabled = raw_log["licks_enabled"]
-#         if licks_enabled is False:
-#             # these assertions dont make sense if licks are disabled
-#             continue
-
-#         if trial_type == "aborted":
-#             if not np.isnan(change_time):  # change_time is nan if change never occurred
-#                 assert has_response_before_window(change_time, lick_times, response_window), \
-#                     f"Aborted trial doesnt have early response. trial index: {idx}"
-#         elif trial_type == "autorewarded":
-#             pass
-#         elif trial_type == "go":
-#             assert not has_response_before_window(change_time, lick_times, response_window), \
-#                 f"Non-aborted go trial has early response. trial index: {idx}"
-#             if response_type == "HIT":
-#                 assert has_response_within_window(change_time, lick_times, response_window), \
-#                     f"Non-aborted go trial has response_type == 'HIT' but doesnt have response within window. trial index: {idx}"
-#         elif trial_type == "catch":
-#             assert not has_response_before_window(change_time, lick_times, response_window), \
-#                 f"Non-aborted catch has early response. trial index: {idx}"
-#             if response_type == "FA":
-#                 assert has_response_within_window(change_time, lick_times, response_window), \
-#                     f"Non-aborted catch trial with response within window not marked as false alarm. trial index: {idx}"
-#         else:
-#             raise Exception("Unexpected trial_type=%s" % trial_type)
-
-
 def test_aborted_trials(raw, extended_trials_df):
     for idx, row in extended_trials_df.iterrows():
         raw_log = raw["items"]["behavior"]["trial_log"][idx]
@@ -70,3 +35,47 @@ def test_aborted_trials(raw, extended_trials_df):
                     f"Aborted trial doesnt have early response. trial index: {idx}"
             assert response_type == "EARLY_RESPONSE", \
                 f"Aborted trial response_type not EARLY_RESPONSE. trial index: {idx}"
+
+
+def test_go_trials(raw, extended_trials_df):
+    response_window = raw["items"]["behavior"]["params"]["response_window"]
+    for idx, row in extended_trials_df.iterrows():
+        raw_log = raw["items"]["behavior"]["trial_log"][idx]
+        trial_type = row["trial_type"]
+        change_time = row["change_time"]
+        lick_times = row["lick_times"]
+        response_type = row["response_type"]
+        licks_enabled = raw_log["licks_enabled"]
+
+        if licks_enabled is False:
+            # these assertions dont make sense if licks are disabled
+            continue
+
+        if trial_type == "go":
+            assert not has_early_response(change_time, lick_times), \
+                f"Non-aborted go trial has early response. trial index: {idx}"
+            if response_type == "HIT":
+                assert has_response_within_window(change_time, lick_times, response_window), \
+                    f"Non-aborted go trial has response_type == 'HIT' but doesnt have response within window. trial index: {idx}"
+
+
+def test_catch_trials(raw, extended_trials_df):
+    response_window = raw["items"]["behavior"]["params"]["response_window"]
+    for idx, row in extended_trials_df.iterrows():
+        raw_log = raw["items"]["behavior"]["trial_log"][idx]
+        trial_type = row["trial_type"]
+        change_time = row["change_time"]
+        lick_times = row["lick_times"]
+        response_type = row["response_type"]
+        licks_enabled = raw_log["licks_enabled"]
+
+        if licks_enabled is False:
+            # these assertions dont make sense if licks are disabled
+            continue
+
+        if trial_type == "catch":
+            assert not has_early_response(change_time, lick_times), \
+                f"Non-aborted catch trial has early response. trial index: {idx}"
+            if response_type == "FA":
+                assert has_response_within_window(change_time, lick_times, response_window), \
+                    f"Non-aborted catch trial with response within window not marked as false alarm. trial index: {idx}"
