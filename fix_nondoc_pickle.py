7e7f57706299eb8bee7bf904ad3026fad62a28be
Chris Mochizuki <chrism@alleninstitute.org>
2023-06-08 14:27:33 -0700

    fix test

--- fix_nondoc_pickle.py
+++ fix_nondoc_pickle.py
@@ -362,5 +362,6 @@ if __name__ == "__main__":
                 target_pickle, args.output_dir)
             logger.info("Fixed pickle saved to: %s" % fixed_pickle_path)
         except Exception as e:
+            print(e)
             logger.error("Failed to fix pickle. target=%s." %
                          (target_pickle, ), exc_info=True)
